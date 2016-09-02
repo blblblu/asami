c5cf818b2f703097f4ab8e8a30633c26721c1583
Sebastian Schulz <sebastian.schulz95@gmail.com>
2016-09-02 20:53:21 +0200

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate rand;
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App};
 
 use std::io;
 use std::cmp::Ordering;
@@ -27,57 +27,41 @@ fn foo() {
 
 fn main() {
 
-    let matches = App::new("My Super Program")
-        .version("1.0")
-        .author("Kevin K. <kbknapp@gmail.com>")
-        .about("Does awesome things")
-        .arg(Arg::with_name("config")
-            .short("c")
-            .long("config")
-            .value_name("FILE")
-            .help("Sets a custom config file")
-            .takes_value(true))
-        .arg(Arg::with_name("INPUT")
-            .help("Sets the input file to use")
-            .required(true)
-            .index(1))
-        .arg(Arg::with_name("v")
+    let matches = App::new("asami")
+        //.version("1.0")
+        .author("Sebastian Schulz <contact@sesc.me>")
+        .about("pixel sorter")
+        .arg(Arg::with_name("input")
+            .short("i")
+            .long("input")
+            .value_name("PATH")
+            .help("path to input file")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("PATH")
+            .help("path to output file")
+            .takes_value(true)
+            .required(true))
+        /*.arg(Arg::with_name("v")
             .short("v")
             .multiple(true)
-            .help("Sets the level of verbosity"))
-        .subcommand(SubCommand::with_name("test")
-            .about("controls testing features")
-            .version("1.3")
-            .author("Someone E. <someone_else@other.com>")
-            .arg(Arg::with_name("debug")
-                .short("d")
-                .help("print debug information verbosely")))
+            .help("Sets the level of verbosity"))*/
         .get_matches();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    let input = matches.value_of("input").unwrap();
+    let output = matches.value_of("output").unwrap();
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    println!("input: {}, output: {}", input, output);
 
     // Vary the output based on how many times the user used the "verbose" flag
     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
+    /*match matches.occurrences_of("v") {
         0 => println!("No verbose info"),
         1 => println!("Some verbose info"),
         2 => println!("Tons of verbose info"),
         3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
-    }
+    }*/
 }
