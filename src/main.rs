741be8e64b8d3c35038d89fa958698519a47a3f1
Sebastian Schulz <sebastian.schulz95@gmail.com>
2016-09-03 16:16:11 +0200

    cli improvements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // extern crate rand;
+#[macro_use]
 extern crate clap;
 extern crate image;
 
@@ -7,12 +8,19 @@ use std::path::Path;
 use clap::{Arg, App};
 use image::{DynamicImage, GenericImage, RgbaImage};
 
+arg_enum!{
+    //#[derive(Debug)]
+    enum Mode {
+        brute
+    }
+}
+
 fn main() {
     let matches = App::new("asami")
         .version("0.1.0")
         .author("Sebastian Schulz <mail@sesc.me>")
         .about("pixel sorter")
-        .arg(Arg::with_name("input")
+        /*.arg(Arg::with_name("input")
             .short("i")
             .long("input")
             .value_name("PATH")
@@ -26,10 +34,27 @@ fn main() {
             .help("path to output file")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("mode")
+            .short("m")
+            .long("mode")
+            .value_name("MODE")
+            .help("path to input file")
+            .takes_value(true)
+            .possible_values(&Mode::variants())
+            .required(true))*/
+        .args_from_usage(
+            "<INPUT> 'Sets the input file to use'
+             <OUTPUT> 'Sets the output file to use'")
+        .arg(Arg::from_usage("-m --mode <MODE> 'Sets the mode to use")
+            .possible_values(&Mode::variants())
+            .default_value("brute"))
         .get_matches();
 
-    let input = matches.value_of("input").unwrap();
-    let output = matches.value_of("output").unwrap();
+    let input = matches.value_of("INPUT").unwrap();
+    let output = matches.value_of("OUTPUT").unwrap();
+
+    let mode = matches.value_of("mode").unwrap();
+    println!("{}", mode);
 
     handle_request(input, output);
 }
@@ -42,12 +67,12 @@ fn handle_request(input: &str, output: &str) {
     println!("dimensions {:?}", in_img.dimensions());
     println!("{:?}", in_img.color());
 
-    let out_img = sort_them_pixels(in_img);
+    let out_img = brute_sort(in_img);
 
     let _ = out_img.save(output).unwrap();
 }
 
-fn sort_them_pixels(input: &DynamicImage) -> RgbaImage {
+fn brute_sort(input: &DynamicImage) -> RgbaImage {
     let mut output = RgbaImage::new(input.width(), input.height());
 
     let mut x = 0;
